0c4518eab93a9dda80595c25712fe7c038f188bb
Davis Owen <sdowen12@gmail.com>
2024-09-27 00:10:21 -0500

    tweaked prompt

--- src/template.ts
+++ src/template.ts
@@ -3,7 +3,7 @@ export const DEFAULT_PROMPT_TEMPLATE = `Classify this content:
 """
 {{input}}
 """
-Answer format is JSON {reliability:0~1, output:selected_category}. 
+Answer format is JSON Array [{reliability:0~1, output:selected_category}, ...]. 
 Even if you are not sure, qualify the reliability and select one. 
 Output must be one of these:
 
