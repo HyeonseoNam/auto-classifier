8ff03089191e8fd4cc7533c4787a7c865b2a280b
Robin Tuszik <mail@robin.gg>
2024-12-13 16:58:51 +0100

    fix: imporove default prompt template to ensure valid JSON output.

--- src/template.ts
+++ src/template.ts
@@ -4,7 +4,8 @@ export const DEFAULT_PROMPT_TEMPLATE = `Classify this content:
 {{input}}
 """
 Answer format is JSON {reliability:0~1, outputs:[tag1,tag2,...]}. 
-Even if you are not sure, qualify the reliability and select the best matches.
+Even if you are unsure, qualify the reliability and select the best matches.
+Respond only with valid JSON. Do not write an introduction or summary.
 Output tags must be from these options:
 
 {{reference}}
@@ -16,5 +17,5 @@ export const DEFAULT_PROMPT_TEMPLATE_WO_REF = `Classify this content:
 """
 Answer format is JSON {reliability:0~1, output:selected_category}. 
 Even if you are not sure, qualify the reliability and recommend a proper category.
-
+Respond only with valid JSON. Do not write an introduction or summary.
 `;
